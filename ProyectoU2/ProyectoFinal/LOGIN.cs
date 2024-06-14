f35dfa9ac7c7403a4121870de65f64d66c72f774
pieroalexanderppc <102675808+pieroalexanderppc@users.noreply.github.com>
2024-06-14 12:31:00 -0500

    Add files via upload

--- ProyectoU2/ProyectoFinal/LOGIN.cs
+++ ProyectoU2/ProyectoFinal/LOGIN.cs
@@ -245,9 +245,9 @@ namespace Proyecto_Final_Blood_Bank
 
 
 
-		public static string NombreUsuario;
+        private static string NombreUsuario;
 
-		public bool Revision(string str)
+        public bool Revision(string str)
 		{
 			return Regex.IsMatch(str, @"^[a-zA-Z0-9]+$");
 		}
